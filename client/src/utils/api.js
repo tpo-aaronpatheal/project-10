d0fb1bd1aa00c586fca12167d11aa92d172998b0
tpo-apatheal <77015964+tpo-apatheal@users.noreply.github.com>
2021-04-13 13:38:10 -0700

    SigIn and api.js update

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,11 +1,25 @@
- import axios from 'axios';
+import axios from 'axios';
+import Context from '../context';
 
- const url = 'http://localhost:5000/api/'
+const url = 'http://localhost:5000/api/'
+
 
 export default {
      
     getAllCourses: async (path) => {
         const response = await axios.get(`${url}${path}`);
         return response;
-     } 
+     },
+     
+    getUser: async (path, email, password) => {
+        const encodedCredentials = btoa(`${email}:${password}`);
+       // options.headers['Authorization'] = `Basic ${encodedCredentials}`;
+        const response = await axios.get(`${url}${path}`, {
+            auth: {
+                username: email,
+                password: password
+            }
+        });
+        console.log(response);
+    }
 }

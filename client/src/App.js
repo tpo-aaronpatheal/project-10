f3584d59648976375e12b3587654ce65b6db6aa8
tpo-tstoll <77018670+tpo-tstoll@users.noreply.github.com>
2021-04-13 11:04:56 -0500

    Adjusted CSS import

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import './styles/global.css';
 import React, { useContext } from 'react';
 import Context from './context';
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from "react-router-dom";
-import './index.css';
+import './styles/reset.css';
+import './styles/global.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { ContextProvider } from './context';
 
 ReactDOM.render(
@@ -20,4 +20,3 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

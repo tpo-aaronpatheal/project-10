474e68148b4d27a7063a77f18603d22051770537
Erica Collier <ercollie@travelers.com>
2021-04-16 11:22:59 -0400

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -10,12 +10,7 @@ import CourseDetail from './components/CourseDetail'
 import Notfound from './components/NotFound'
 import CreateCourse from './components/CreateCourse';
 import DeleteCourse from './components/DeleteCourse';
-<<<<<<< HEAD
-import Forbidden from './components/Forbidden'
-import UnhandledError from './components/UnhandledError'
-=======
 import PrivateRoute from './components/PrivateRoute';
->>>>>>> 978b397f47a87f8d447ad509a95ff409f6331842
 
 function App() {
 
@@ -23,38 +18,6 @@ function App() {
     <>
       <Nav />
       <Switch>
-<<<<<<< HEAD
-        <Route exact path='/'>
-          <AllCourses />
-        </Route>
-        <Route exact path='/signin'>
-          <SignIn />
-        </Route>
-        <Route exact path='/signup'>
-          <SignUp />
-        </Route>
-        <Route exact path='/signout'>
-          <SignOut />
-        </Route>
-        <Route exact path='/courses/create'>
-          <CreateCourse />
-        </Route>
-        <Route exact path={`/courses/:id`}>
-          <CourseDetail />
-        </Route>
-        <Route exact path={'/courses/:id/delete'}>
-          <DeleteCourse />
-        </Route>
-        <Route exact path={'/error'}>
-          <UnhandledError />
-        </Route>
-        <Route exact path={'/forbidden'}>
-          <UnhandledError />
-        </Route>
-        <Route>
-          <Notfound />
-        </Route>
-=======
         <Route exact path='/' component={AllCourses} />
         <Route exact path='/signin' component={SignIn} />
         <Route exact path='/signup' component={SignUp} />
@@ -63,7 +26,6 @@ function App() {
         <Route exact path={`/courses/:id`} component={CourseDetail}/>
         <Route exact path={'/courses/:id/delete'} component={DeleteCourse}/>
         <Route component={Notfound} />
->>>>>>> 978b397f47a87f8d447ad509a95ff409f6331842
       </Switch>
     </>
   );
